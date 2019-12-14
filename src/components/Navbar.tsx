c0ba72eece71650a13d6dd03d35c1aea2b5c191f
= <=>
2019-12-14 20:22:29 +0300

    Revert "navbar component edited. drawer added and icons are customized."
    This reverts commit e4e0fdbe0686c0d066b94c845e587fbfdd405f87.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,10 +15,8 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import HelpIcon from '@material-ui/icons/Help';
-import ContactsIcon from '@material-ui/icons/Contacts';
-import BookmarksIcon from '@material-ui/icons/Bookmarks';
-import PublishIcon from '@material-ui/icons/Publish';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const drawerWidth = 240;
 
@@ -111,7 +109,7 @@ export default function PersistentDrawerLeft() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            {`Excel Veri Görselleştiricisi`}
+            Persistent drawer
           </Typography>
         </Toolbar>
       </AppBar>
@@ -131,29 +129,21 @@ export default function PersistentDrawerLeft() {
         </div>
         <Divider />
         <List>
-          {['Excel Yükle'].map((text, index) => (
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon><PublishIcon /></ListItemIcon>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
         </List>
         <Divider />
         <List>
-            <ListItem button key={`Nasıl Kullanılır?`}>
-              <ListItemIcon><HelpIcon/></ListItemIcon>
-              <ListItemText primary={`Nasıl Kullanılır?`} />
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
             </ListItem>
-
-            <ListItem button key={`İletişim`}>
-            <ListItemIcon><ContactsIcon/></ListItemIcon>
-            <ListItemText primary={`İletişim`} />
-            </ListItem>
-
-            <ListItem button key={`SSS`}>
-            <ListItemIcon><BookmarksIcon/></ListItemIcon>
-            <ListItemText primary={`SSS`} />
-            </ListItem>
+          ))}
         </List>
       </Drawer>
       <main
@@ -166,13 +156,24 @@ export default function PersistentDrawerLeft() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
           facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quis   que non tellus. Convallis convallis tellus id interdum velit laoreet id
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
           donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
           adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
           Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
           imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
           arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
           donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
         </Typography>
       </main>
     </div>
