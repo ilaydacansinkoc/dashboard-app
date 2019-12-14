1600cd455dd500de92d9d85afa2060d997d61c11
= <=>
2019-12-14 18:25:50 +0300

    Navbar component template created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import {Button} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+export default function Navbar() {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon/>
+          </IconButton>
+          <Typography variant="subtitle1" className={classes.title}>
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+
+}
