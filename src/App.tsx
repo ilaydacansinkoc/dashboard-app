737353f092dd3f661745561ce2c5f6dfdec63944
= <=>
2019-12-14 17:47:16 +0300

    unused imports and files are removed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.PureComponent {
   render(){
-    return (`App is here`);
+    return (
+      <div> Hello, World!</div>
+    );
   }
 }
 export default App;
