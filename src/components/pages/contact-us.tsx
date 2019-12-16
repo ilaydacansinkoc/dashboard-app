dd10bb88150218ad7d01f2399d2948b301a9f02f
= <=>
2019-12-16 23:32:53 +0300

    contact us page template updated.

--- src/components/pages/contact-us.tsx
+++ src/components/pages/contact-us.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+import {createStyles, Paper, Theme} from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
-class ContactUs extends React.PureComponent{
-  render(){
-    return(<div>
-      {`İletişime Geçin Sayfası`}
-    </div>)
-  };
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+
+export default function ContactUs(){
+  const classes = useStyles();
+  return(
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>Contact Us</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-export default ContactUs;
