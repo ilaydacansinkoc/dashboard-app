bfcbd80fecd2deb4105b1b29e840a54c47763a52
= <=>
2019-12-19 00:15:46 +0300

    navbar component changed to responsive one. permanent in desktop view and closed in mobile view. and images are imported as well. style transformation also required.

--- src/components/interfaces/Navbar.tsx
+++ src/components/interfaces/Navbar.tsx
@@ -1,71 +1,56 @@
 import React from 'react';
 // tslint:disable-next-line:no-implicit-dependencies
 import clsx from 'clsx';
-import {Drawer, CssBaseline, AppBar, Toolbar, List, Typography, Divider, IconButton, ListItem,
-  ListItemIcon, ListItemText, makeStyles, useTheme} from '@material-ui/core';
-import { Menu, ChevronLeft, ChevronRight, Help, Contacts, Bookmarks, Publish} from '@material-ui/icons';
+import {
+  Drawer, CssBaseline, AppBar, Toolbar, List, Typography, Divider, IconButton, ListItem,
+  ListItemIcon, ListItemText, makeStyles, useTheme, Theme, createStyles, Hidden
+} from '@material-ui/core';
+import { Menu, Help, Contacts, Bookmarks, Publish} from '@material-ui/icons';
 import FrequentlyAsked from '../pages/frequently-asked';
 import HowSiteWorks from '../pages/how-site-works';
 import ContactUs from '../pages/contact-us';
 import UploadExcel from '../pages/upload-excel';
+import drawerImage from '../../resources/drawerImage1.png';
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex'
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  hide: {
-    display: 'none'
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end'
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  }
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex'
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0
+      }
+    },
+    appBar: {
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth
+      }
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none'
+      }
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+      backgroundImage: 'url(' + drawerImage + ')',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      zIndex: 1
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3)
+    }
+  })
+);
 
 export default function PersistentDrawerLeft() {
   const classes = useStyles();
@@ -106,22 +91,49 @@ export default function PersistentDrawerLeft() {
     setFrequenlyAskedClicked(true);
   };
 
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        <ListItem button={true} key={'Excel Yükle'} onClick={handleFileUploadItem}>
+          <ListItemIcon>
+            <Publish/>
+          </ListItemIcon>
+          <ListItemText primary={'Excel Yükle'}/>
+        </ListItem>
+      </List>
+      <Divider/>
+      <List>
+        <ListItem button={true} key={`Nasıl Kullanılır?`} onClick={handleHowItWorksItem}>
+          <ListItemIcon><Help/></ListItemIcon>
+          <ListItemText primary={`Nasıl Kullanılır?`}/>
+        </ListItem>
+
+        <ListItem button={true} key={`İletişim`} onClick={handleContactUsItem}>
+          <ListItemIcon><Contacts/></ListItemIcon>
+          <ListItemText primary={`İletişim`}/>
+        </ListItem>
+
+        <ListItem button={true} key={`SSS`} onClick={handleFrequentlyAskedItem}>
+          <ListItemIcon><Bookmarks/></ListItemIcon>
+          <ListItemText primary={`SSS`}/>
+        </ListItem>
+      </List>
+    </div>
+  );
+
   return (
     <div className={classes.root}>
-      <CssBaseline/>
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open
-        })}
-      >
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerOpen}
             edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
+            onClick={handleDrawerOpen}
+            className={classes.menuButton}
           >
             <Menu/>
           </IconButton>
@@ -131,55 +143,43 @@ export default function PersistentDrawerLeft() {
         </Toolbar>
       </AppBar>
 
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeft/> : <ChevronRight/>}
-          </IconButton>
-        </div>
-        <Divider/>
-        <List>
-          <ListItem button={true} key={'Excel Yükle'} onClick={handleFileUploadItem}>
-            <ListItemIcon>
-              <Publish/>
-            </ListItemIcon>
-            <ListItemText primary={'Excel Yükle'}/>
-          </ListItem>
-        </List>
-        <Divider/>
-        <List>
-          <ListItem button={true} key={`Nasıl Kullanılır?`} onClick={handleHowItWorksItem}>
-            <ListItemIcon><Help/></ListItemIcon>
-            <ListItemText primary={`Nasıl Kullanılır?`}/>
-          </ListItem>
-
-          <ListItem button={true} key={`İletişim`} onClick={handleContactUsItem}>
-            <ListItemIcon><Contacts/></ListItemIcon>
-            <ListItemText primary={`İletişim`}/>
-          </ListItem>
-
-          <ListItem button={true} key={`SSS`} onClick={handleFrequentlyAskedItem}>
-            <ListItemIcon><Bookmarks/></ListItemIcon>
-            <ListItemText primary={`SSS`}/>
-          </ListItem>
-        </List>
-      </Drawer>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp={true} implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={'left'}
+            open={open}
+            onClose={handleDrawerClose}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown={true} implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open={true}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
 
       <main
         className={clsx(classes.content, {
-          [classes.contentShift]: open
+          [classes.content]: open
         })}
         onClickCapture={handleDrawerClose}
       >
-        <div className={classes.drawerHeader}/>
+        <div className={classes.toolbar}/>
         <Typography paragraph={true}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
